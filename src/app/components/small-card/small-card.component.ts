49a169191a9acbfa2be58c82a7be750ce9cbe73d
Veronica <almeida.vrc@gmail.com>
2023-09-05 16:52:10 -0300

    comments dinamycs properties

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.sass']
+  styleUrls: ['./small-card.component.sass'],
 })
 export class SmallCardComponent {
+  @Input()
+  photoCover: string = '';
 
+  @Input()
+  cardTitle: string = '';
 }
